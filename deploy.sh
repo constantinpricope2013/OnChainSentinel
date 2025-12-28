6939515e813ab9274a4d0a64f752f0633e5fc2eb
cpr <constantinpricope2013@gmail.com>
2025-12-28 17:31:55 +0200

    destroy/deploy tested

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,9 @@
 
 # Step 1. Create env
 confluent environment create onchainsentinel
-echo "✅ Step 1. Environment Created"
+export ENV_ID=$(confluent environment list -o json | jq -r '.[] | select(.name=="onchainsentinel") | .id')
+confluent environment use $ENV_ID
+echo "✅ Step 1. Environment $ENV_ID Created"
 
 
 # Step 2 Create cluste 
@@ -15,7 +17,7 @@ confluent kafka cluster create onchainsentinel-kafka \
 export KAFKA_CLUSTER_ID=$(confluent kafka cluster list -o json \
   | jq -r 'map(select(.name=="onchainsentinel-kafka"))[0].id')
 confluent kafka cluster use $KAFKA_CLUSTER_ID
-echo "✅ Step 2. Kafka Cluster Created"
+echo "✅ Step 2. Kafka Cluster $KAFKA_CLUSTER_ID Created"
 
 
 # Step 3. Create Flink compute pool 
@@ -25,5 +27,5 @@ export FLINK_POOL_ID=$(
     | jq -r '.[] | select(.name=="onchainsentinel-flink") | .id'
 )
 confluent flink compute-pool use $FLINK_POOL_ID
-echo "✅ Step 2. Flink compute pool created"
+echo "✅ Step 2. Flink compute pool $FLINK_POOL_ID created"
 
