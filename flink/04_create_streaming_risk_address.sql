6f8131476470289f25799e3526c0e931d313e747
cpr <constantinpricope2013@gmail.com>
2025-12-28 20:08:27 +0200

    event producer added

--- flink/04_create_streaming_risk_address.sql
+++ flink/04_create_streaming_risk_address.sql
@@ -1,3 +1,10 @@
+
+-- Not working due to upsert
+-- Error: can't fetch results. Statement phase is: FAILED
+-- Error details: Table sink 'onchainsentinel.lkc-go56j1.onchainsentinel_risk_address' doesn't support consuming update changes which is produced by node GroupAggregate(groupBy=[address, chain, model_name], select=[address, chain, model_name, MAX(risk) AS risk, ARRAY_AGG(reason) AS $f4]
+
+
+
 INSERT INTO onchainsentinel_risk_address
 SELECT
     CAST(address AS BYTES) AS key,
